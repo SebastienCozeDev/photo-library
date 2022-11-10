8488df8350396f992f61c696fee2f99aeb734802
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-11-10 22:28:24 +0100

    Mise à jour du router pour qu'il implémente le controller.

--- routes/album.routes.js
+++ routes/album.routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const albumController = require('../controllers/album.controller');
 
 /**
  * Route vers la création d'un album.
  */
-router.get('/albums/create', (req, res) => {
-    res.render('new-album', { title: 'Nouvel album' });
-});
+router.get('/albums/create', albumController.createAlbumForm);
+
+/**
+ * Route qui traite le formulaire de création d'album.
+ */
+router.post('/albums/create', albumController.createAlbum);
 
 module.exports = router;
