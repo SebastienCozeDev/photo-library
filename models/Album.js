ae6fc31038a8d44655e843e28f6b76a7ec467308
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-11-19 14:42:28 +0100

    Ajout de l'attribut user pour l'utilisateur.

--- models/Album.js
+++ models/Album.js
@@ -4,7 +4,12 @@ const mongoose = require('mongoose');
  * Schéma pour un album.
  */
 const albumSchema = new mongoose.Schema({
-    title: { type: String, required: true }
+    title: { type: String, required: true },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Album', albumSchema);
